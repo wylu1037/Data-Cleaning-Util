d29bfb8361fb115866a6b09efafc5cf9346c6f26
wylu <wylu@zkjg.com>
2022-08-26 08:33:28 +0800

    feat: 删除链和节点的redis缓存

--- service/chain_service.go
+++ service/chain_service.go
@@ -3,31 +3,87 @@ package service
 import (
 	"baas-clean/model"
 	"baas-clean/utils"
+	"fmt"
 	"strconv"
 )
 
 const (
-	ChainNode     = "CHAIN_NODE:"
+	ChainNode     = "CHAIN_NODE:" // 以下是db7
 	NodeVote      = "NODE_VOTE:"
 	NodeVoteTally = "NODE_VOTE_TALLY:"
 
-	//
+	BlockLtc = "BLOCK-LTC:ZLTC" // 以下是db8
+	InfoLtc  = "INFO-LTC:ZLTC"
+
+	BlockLtc2  = "BLOCK-LTC:ZLTC" // 以下是db9
+	InfoLtc2   = "INFO-LTC:ZLTC"
+	ChainNode2 = "CHAIN-NODE:CHAIN:NODE:STATUS"
 )
 
 // ChainDelete 删除链信息
 func ChainDelete(chainId uint64) {
 
-	// 删除缓存
-	utils.LikeDelete(ChainNode+strconv.FormatUint(chainId, 10), 7)
+	// 删除链相关缓存
+	var err error
+	chainIdStr := strconv.FormatUint(chainId, 10)
+	err = utils.LikeDelete(ChainNode+chainIdStr, 7)
+	if err != nil {
+		fmt.Printf("chain_service delete redis cache occured error, key is %s%s, error message is %v \n",
+			ChainNode, chainIdStr, err)
+		return
+	}
+	err = utils.LikeDelete(BlockLtc+chainIdStr, 8)
+	if err != nil {
+		fmt.Printf("chain_service delete redis cache occured error, key is %s%s, error message is %v \n",
+			BlockLtc, chainIdStr, err)
+		return
+	}
+	err = utils.LikeDelete(InfoLtc+chainIdStr, 8)
+	if err != nil {
+		fmt.Printf("chain_service delete redis cache occured error, key is %s%s, error message is %v \n",
+			InfoLtc, chainIdStr, err)
+		return
+	}
+	err = utils.LikeDelete(BlockLtc2+chainIdStr, 8)
+	if err != nil {
+		fmt.Printf("chain_service delete redis cache occured error, key is %s%s, error message is %v \n",
+			BlockLtc2, chainIdStr, err)
+		return
+	}
+	err = utils.LikeDelete(InfoLtc2+chainIdStr, 8)
+	if err != nil {
+		fmt.Printf("chain_service delete redis cache occured error, key is %s%s, error message is %v \n",
+			InfoLtc2, chainIdStr, err)
+		return
+	}
+	err = utils.LikeDelete(ChainNode2+chainIdStr, 8)
+	if err != nil {
+		fmt.Printf("chain_service delete redis cache occured error, key is %s%s, error message is %v \n",
+			ChainNode2, chainIdStr, err)
+		return
+	}
 
 	// 查找链关联的节点
 	nodes := model.FindNodesByChainId(chainId)
 	for _, node := range *nodes {
-		utils.LikeDelete(NodeVote+strconv.FormatUint(node.ID, 10), 7)
-		utils.LikeDelete(NodeVoteTally+strconv.FormatUint(node.ID, 10), 7)
+		// 删除节点相关缓存
+		nodeIdStr := strconv.FormatUint(node.ID, 10)
+		err = utils.LikeDelete(NodeVote+nodeIdStr, 7)
+		if err != nil {
+			fmt.Printf("chain_service delete redis cache occured error, key is %s%s, error message is %v \n",
+				NodeVote, nodeIdStr, err)
+			return
+		}
+		err := utils.LikeDelete(NodeVoteTally+strconv.FormatUint(node.ID, 10), 7)
+		if err != nil {
+			fmt.Printf("chain_service delete redis cache occured error, key is %s%s, error message is %v \n",
+				NodeVoteTally, nodeIdStr, err)
+			return
+		}
 	}
 
-	// model.DeleteChainById(chainId)
+	// 删除链数据
+	model.DeleteChainById(chainId)
 
 	// 查找链关联的节点
 
