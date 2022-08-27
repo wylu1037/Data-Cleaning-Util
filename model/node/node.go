af322fc1015e083daa63d2acf79e62c1f2395532
卢文扬 <2052987369@qq.com>
2022-08-27 18:42:42 +0800

    feat: 超级账本节点查询删除

--- model/node/node.go
+++ model/node/node.go
@@ -2,15 +2,13 @@ package node
 
 import (
 	"clear-chain/config"
-	"encoding/json"
-	"fmt"
 	"github.com/sirupsen/logrus"
 )
 
 type Node struct {
-	ID      uint64 // nodeId
+	ID      uint64
 	Name    string
-	ChainId uint64 // chainId
+	ChainId uint64
 	IP      string
 }
 
@@ -22,9 +20,6 @@ func (n Node) TableName() string {
 func FindNodesByChainId(chainId uint64) *[]Node {
 	var nodes []Node
 	config.MySqlDB.Where("chainId = ?", chainId).Find(&nodes)
-
-	nodesData, _ := json.Marshal(nodes)
-	fmt.Println(string(nodesData))
 
 	return &nodes
 }
@@ -41,4 +36,40 @@ func deleteNodeById(nodeId uint64) {
 	logrus.Infof("[node] deleteNodeById() called with: nodeId = %d", nodeId)
 
 	config.MySqlDB.Delete(&Node{}, nodeId).Limit(1)
+}
+
+type HyperledgerNode struct {
+	ID         uint64
+	ChainId    uint64
+	OrgId      uint64
+	HostId     uint64
+	NodeType   uint8
+	NodeUserId uint64
+}
+
+func (HyperledgerNode) TableName() string {
+	return "fabric_chain_node"
+}
+
+// FindHyperledgerNodesByChainId 根据链查找超级账本节点
+func FindHyperledgerNodesByChainId(chainId uint64) []HyperledgerNode {
+	logrus.Infof("[node] FindHyperledgerNodesByChainId() called with: chainId = %d", chainId)
+
+	var nodes []HyperledgerNode
+	config.MySqlDB.Where("chainId = ? and deleteFlag = ?", chainId, 0).Find(&nodes)
+
+	return nodes
+}
+
+// DeleteHyperledgerNodes 批量删除超级账本节点
+func DeleteHyperledgerNodes(nodes []HyperledgerNode) {
+	for _, nodeInfo := range nodes {
+		deleteHyperledgerNode(nodeInfo.ID)
+	}
+}
+
+func deleteHyperledgerNode(nodeId uint64) {
+	logrus.Infof("[node] deleteHyperledgerNode() called with: nodeId = %d", nodeId)
+
+	config.MySqlDB.Delete(&HyperledgerNode{}, nodeId)
 }
