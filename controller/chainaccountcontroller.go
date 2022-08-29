8d5329c35f82aca462995136068268afa7c2f3e6
wylu <wylu@zkjg.com>
2022-08-29 08:27:07 +0800

    chore: go.sum

--- controller/chainaccountcontroller.go
+++ controller/chainaccountcontroller.go
@@ -3,9 +3,9 @@ package controller
 import "github.com/gin-gonic/gin"
 
 func ChainAccountRoute(e *gin.Engine) {
-	e.POST("/chain/account/importAdmin")
+	e.POST("/chain/account/importAdmin", importAdminAccountHandler)
 }
 
-func importAdminAccount() {
+func importAdminAccountHandler(c *gin.Context) {
 
 }
